cd0af7daee58ef2a6a5282ffadd9e700e1169f6a
Adi Mor <Adi Mor@DESKTOP-NT3LJFC>
2022-01-11 19:25:03 +0200

    add Company Object

---
+++ CoPay/CoPayApi/Data/Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CoPayApi.Data.Entities
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public int PrivateCompanyID { get; set; }
+        public int Id { get; set; }
+        public List<User> UsersInTheCompany { get; set; }
+    }
+}
